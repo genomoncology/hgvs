0dd34c219123e7fd3bc169034e048bf8a59c501f
Brendan O'Donnell <brendan@odonnell.xyz>
2026-06-02 16:41:36 -0400

    Add test cases

--- tests/issues/test_829.py
+++ tests/issues/test_829.py
@@ -9,7 +9,9 @@ import pytest
     ('NM_004448.4:c.2339dup', 'NP_004439.2:p.(Tyr781IlefsTer93)'),
     ('NM_024529.5:c.1595_*2delinsCCCC', 'NP_078805.3:p.(Ter532SerextTer29)'),
     ('NM_004985.5:c.567_*1delinsTC', 'NP_004976.2:p.(Ter189TyrextTer8)'),
-    ('NM_004985.5:c.567_*1insTAGAAA', 'NP_004976.2:p.?')
+    ('NM_004985.5:c.567_*1insTAGAAA', 'NP_004976.2:p.?'),
+    ('NM_000203.4:c.1960T>C', 'NP_000194.2:p.(Ter654ArgextTer?)'),
+    ('NM_000203.4:c.1730del', 'NP_000194.2:p.(Cys577SerfsTer?)'),
 ])
 def test_c_to_p(var_c_str, var_p_str, parser, am37):
     var_c = parser.parse(var_c_str)
